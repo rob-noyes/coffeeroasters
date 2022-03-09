e87301e0b3991f4937b572797cf55007b229df20
Rob Noyes <robertjnoyes1@gmail.com>
2022-03-09 08:44:38 -0800

    header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
